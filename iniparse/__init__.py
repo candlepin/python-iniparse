e6843656d6c8f98fda1541b91f993007e64038fe
Paramjit Oberoi <param@cs.wisc.edu>
2007-07-14 15:39:51 +0000

    Export the classes that users need in iniparse/__init__.py so that user code doen't need to worry about sub-modules.
    git-svn-id: file:///home/jiri/github/candlepin/svn-python-iniparse/trunk@38 6890172c-e1a9-44ee-b606-0410448aa431

---
+++ iniparse/__init__.py
@@ -0,0 +1,8 @@
+from ini import INIConfig
+from config import BasicConfig, ConfigNamespace
+from compat import RawConfigParser, ConfigParser, SafeConfigParser
+
+__all__ = [
+    'INIConfig', 'BasicConfig', 'ConfigNamespace',
+    'RawConfigParser', 'ConfigParser', 'SafeConfigParser',
+]
