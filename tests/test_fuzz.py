479bf428d894b12ff488c832c8b06d4b7ec0dc02
Paramjit Oberoi <param@cs.wisc.edu>
2009-03-18 03:39:34 +0000

    make fuzz test dump out the problematic ini file on failure
    git-svn-id: file:///home/jiri/github/candlepin/svn-python-iniparse/trunk@121 6890172c-e1a9-44ee-b606-0410448aa431

--- tests/test_fuzz.py
+++ tests/test_fuzz.py
@@ -73,37 +73,46 @@ class test_fuzz(unittest.TestCase):
             num_iter = int(os.environ['INIPARSE_FUZZ_ITERATIONS'])
         except (KeyError, ValueError):
             num_iter = 100
-        for i in range(num_iter):
-            # parse random file with errors disabled
-            s = random_ini_file()
-            c = ini.INIConfig(parse_exc=False)
-            c._readfp(StringIO(s))
-            # check that file is preserved, except for
-            # commenting out erroneous lines
-            l1 = s.split('\n')
-            l2 = str(c).split('\n')
-            self.assertEqual(len(l1), len(l2))
-            good_lines = []
-            for i in range(len(l1)):
-                try:
-                    self.assertEqual(l1[i], l2[i])
-                    good_lines.append(l1[i])
-                except AssertionError:
-                    self.assertEqual('#'+l1[i], l2[i])
-            # parse the good subset of the file
-            # using ConfigParser
-            s = '\n'.join(good_lines)
-            cc = compat.RawConfigParser()
-            cc.readfp(StringIO(s))
-            cc_py = ConfigParser.RawConfigParser()
-            cc_py.readfp(StringIO(s))
-            # compare the two configparsers
-            self.assertEqualSorted(cc_py.sections(), cc.sections())
-            self.assertEqualSorted(cc_py.defaults().items(), cc.defaults().items())
-            for sec in cc_py.sections():
-                self.assertEqualSorted(cc_py.options(sec), cc.options(sec))
-                for opt in cc_py.options(sec):
-                    self.assertEqual(cc_py.get(sec, opt), cc.get(sec, opt))
+        for fuzz_iter in range(num_iter):
+            try:
+                # parse random file with errors disabled
+                s = random_ini_file()
+                c = ini.INIConfig(parse_exc=False)
+                c._readfp(StringIO(s))
+                # check that file is preserved, except for
+                # commenting out erroneous lines
+                l1 = s.split('\n')
+                l2 = str(c).split('\n')
+                self.assertEqual(len(l1), len(l2))
+                good_lines = []
+                for i in range(len(l1)):
+                    try:
+                        self.assertEqual(l1[i], l2[i])
+                        good_lines.append(l1[i])
+                    except AssertionError:
+                        self.assertEqual('#'+l1[i], l2[i])
+                # parse the good subset of the file
+                # using ConfigParser
+                s = '\n'.join(good_lines)
+                cc = compat.RawConfigParser()
+                cc.readfp(StringIO(s))
+                cc_py = ConfigParser.RawConfigParser()
+                cc_py.readfp(StringIO(s))
+                # compare the two configparsers
+                self.assertEqualSorted(cc_py.sections(), cc.sections())
+                self.assertEqualSorted(cc_py.defaults().items(), cc.defaults().items())
+                for sec in cc_py.sections():
+                    self.assertEqualSorted(cc_py.options(sec), cc.options(sec))
+                    for opt in cc_py.options(sec):
+                        self.assertEqual(cc_py.get(sec, opt), cc.get(sec, opt))
+            except AssertionError:
+                fname = 'fuzz-test-iter-%d.ini' % fuzz_iter
+                print 'Fuzz test failed at iteration', fuzz_iter
+                print 'Writing out failing INI file as', fname
+                f = open(fname, 'w')
+                f.write(s)
+                f.close()
+                raise
 
     def assertEqualSorted(self, l1, l2):
         l1.sort()
