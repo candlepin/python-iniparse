379db07a208bf7cadb7c12ceff2f761d26356880
Paramjit Oberoi <param@cs.wisc.edu>
2010-05-07 05:46:28 +0000

    the initial version of this was totally broken in many ways
    git-svn-id: file:///home/jiri/github/candlepin/svn-python-iniparse/trunk@136 6890172c-e1a9-44ee-b606-0410448aa431

--- tests/test_multiprocessing.py
+++ tests/test_multiprocessing.py
@@ -7,29 +7,21 @@ except ImportError:
 
 from iniparse import compat, ini
 
-def get_x_dot_y(readfn, writefn):
-    cfg = readfn(timeout=3)
-    writefn(cfg.x.y)
-
 class test_ini(unittest.TestCase):
     """Test sending INIConfig objects."""
 
     def test_queue(self):
+        def getxy(q, w):
+            cfg = q.get_nowait()
+            w.put(cfg.x.y)
         cfg = ini.INIConfig()
-        cfg.x.y = 42
-        q1 = Queue()
-        q2 = Queue()
-        p = Process(target=get_x_dot_y, args=(q1.get, q2.put))
-        q1.put(cfg)
-        self.assertEqual(q2.get(timeout=3), 42)
-
-    def test_pipe(self):
-        cfg = ini.INIConfig()
-        cfg.x.y = 42
-        c1, c2 = Pipe()
-        p = Process(target=get_x_dot_y, args=(c1.recv ,c2.send))
-        c1.send(cfg)
-        self.assertEqual(c2.recv(timeout=3), 42)
+        cfg.x.y = '42'
+        q = Queue()
+        w = Queue()
+        q.put(cfg)
+        p = Process(target=getxy, args=(q, w))
+        p.start()
+        self.assertEqual(w.get(timeout=1), '42')
 
 class suite(unittest.TestSuite):
     def __init__(self):
