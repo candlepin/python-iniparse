28dddd6f45fb5928d1477d14fac5daca92ffbb4c
Matyas Horky <mhorky@redhat.com>
2022-10-17 14:49:03 +0200

    Drop Python 2 code
    Remove conditional imports that try to import Python 2 or Python 3 bits,
    depending on the version.

--- runtests.py
+++ runtests.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 
-import sys
 import tests
 
+
 if __name__ == '__main__':
-    if len(sys.argv) == 2 and sys.argv[1] == '-g':
-        import unittestgui
-        unittestgui.main('tests.Suite')
-    else:
-        import unittest
-        unittest.main(defaultTest='tests.Suite')
+    import unittest
+    unittest.main(defaultTest='tests.Suite')
